65fff0f26eb0db7c674e1297418a16fc93a9d843
Unknown <kevinrbhasin@gmail.com>
2018-08-11 02:29:55 -0400

    build changes

--- example-build-run.sh
+++ example-build-run.sh
@@ -9,25 +9,26 @@ IFS=$'\n\t'
 DEPLOY_DIR=/var/www/rocket.chat
 
 ### BUILD
-meteor npm install
-meteor npm run postinstall
+cd /ic
+sudo meteor npm install
+sudo meteor npm run postinstall
 
 # on the very first build, meteor build command should fail due to a bug on emojione package (related to phantomjs installation)
 # the command below forces the error to happen before build command (not needed on subsequent builds)
 set +e
-meteor add rocketchat:lib
+sudo meteor add rocketchat:lib
 set -e
 
-meteor build --server-only --directory $DEPLOY_DIR
+sudo meteor build --server-only --directory $DEPLOY_DIR
 
 ### RUN
-cd $DEPLOY_DIR/bundle/programs/server
-npm install
+# cd $DEPLOY_DIR/bundle/programs/server
+# sudo npm install
 
-cd $DEPLOY_DIR/bundle
-NODE_ENV=production \
-PORT=3000 \
-ROOT_URL=http://localhost:3000 \
-MONGO_URL=mongodb://localhost:27017/rocketchat \
-MONGO_OPLOG_URL=mongodb://localhost:27017/local \
-node main.js
+# cd $DEPLOY_DIR/bundle
+# NODE_ENV=production \
+# PORT=3000 \
+# ROOT_URL=http://localhost:3000 \
+# MONGO_URL=mongodb://localhost:27017/rocketchat \
+# MONGO_OPLOG_URL=mongodb://localhost:27017/local \
+# node main.js
